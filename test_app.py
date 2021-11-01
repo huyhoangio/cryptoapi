2dd96bbbb76d06fe99d21d090b86647dbe4702d4
Huy Hoang <huy@huyhoang.io>
2021-10-31 20:27:50 -0500

    change localhost to 127.0.0.1

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 import requests
 
-LOCAL_TEST_CONTAINER = '127.0.0.1:5000'
+LOCAL_TEST_CONTAINER = 'http://127.0.0.1:5000'
 
 def test_should_fail_if_api_not_respond():
 	r = requests.get("{}/health".format(LOCAL_TEST_CONTAINER))
