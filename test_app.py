d8e3783c421d5ac8b2ffe8b0f18a08fc25482b57
Huy Hoang <huy@huyhoang.io>
2021-10-31 19:08:34 -0500

    modify test

--- test_app.py
+++ test_app.py
@@ -7,17 +7,25 @@ def test_should_fail_if_api_not_respond():
 
 	assert r.status_code == 200
 
-def test_should_fail_if_currency_not_supported_by_our_api();
+
+def test_should_fail_if_currency_not_supported_by_our_api():
 	r = requests.get("{}/VND".format(LOCAL_TEST_CONTAINER))
 
 	assert r.status_code == 404
+
 
 def test_should_fail_if_currency_not_supported_by_coinbase_api():
 	r = requests.get("{}/XYZ".format(LOCAL_TEST_CONTAINER))
 
 	assert r.status_code == 404
 
-def test_should_fail_if_coinbase_data
+
+def test_should_fail_if_can_not_parse_coinbase_data():
+	r = requests.get("{}/JPY".format(LOCAL_TEST_CONTAINER))
+
+	assert 'data' in r.json() or 'error' in r.json()
+	
+
 def test_should_pass_if_legit_currency_passed_in():
 	r = requests.get("{}/USD".format(LOCAL_TEST_CONTAINER))
 
